2aae108c278e1380a1797e2f638b43653d7e7e69
Vitan <vitan.me@gmail.com>
2019-11-18 16:30:58 +0800

    Sort files

--- UninstallSoftWare/Office_Uninstall.bat
+++ UninstallSoftWare/Office_Uninstall.bat
@@ -1,4 +1,4 @@
-@ECHO OFF&PUSHD %~DP0 &TITLE GRGInstall -- Jay
+@ECHO OFF&PUSHD %~DP0 &TITLE Office Uninstall
 mode con cols=58 lines=25
 color 2F
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
